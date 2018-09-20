ebb2e01442651313943ed2f81d3017968c349eba
Konstantin Lobov <kos33rd@gmail.com>
2018-09-20 20:36:54 +0300

    Week 3 - React support added

--- Week-3/project-example/src/index.js
+++ Week-3/project-example/src/index.js
@@ -1,2 +1,14 @@
-import log from './src'
-log();
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const MyFirstComponent = () => {
+  return (
+    <div>
+      <h1>Hello from React!</h1>
+      <span>I'm just common HTML</span>
+    </div>
+  )
+};
+
+ReactDOM.render(<MyFirstComponent/>, document.getElementById('app'));
