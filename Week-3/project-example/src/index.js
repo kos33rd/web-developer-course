257396fa4edd82ab774e6e3fa8dd2db6ff59f587
Konstantin Lobov <kos33rd@gmail.com>
2018-09-20 21:46:05 +0300

    Week 3 - Sample project props example

--- Week-3/project-example/src/index.js
+++ Week-3/project-example/src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
-const MyFirstComponent = () => {
+const MyFirstComponent = function (props) {
   return (
     <div>
-      <h1>Hello from React!</h1>
+      <h1>{props.title}</h1>
       <span>I'm just common HTML</span>
     </div>
   )
 };
 
-ReactDOM.render(<MyFirstComponent/>, document.getElementById('app'));
+ReactDOM.render(
+  <MyFirstComponent title="Hello from React!"/>,
+  document.getElementById('app')
+);
