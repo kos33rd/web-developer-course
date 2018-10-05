d933415dd01ea7d845863e6aa9096d03362785d1
Лобов Константин Евгеньевич <Lobov.K.Evg@sberbank.ru>
2018-10-05 17:45:53 +0300

    Week-5 workshop - Language select

--- Week-5/project-example/src/news/search.jsx
+++ Week-5/project-example/src/news/search.jsx
@@ -1,6 +1,11 @@
 import TextField from "@material-ui/core/TextField/TextField";
 import {Field, reduxForm} from 'redux-form'
 import React from "react";
+import Select from "@material-ui/core/Select/Select";
+import MenuItem from "@material-ui/core/MenuItem/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel/InputLabel";
+import Input from "@material-ui/core/Input/Input";
+import FormHelperText from "@material-ui/core/FormHelperText/FormHelperText";
 
 
 const AdaptedTextField = ({input: {value, onChange}, ...custom}) => (
@@ -11,10 +16,23 @@ const AdaptedTextField = ({input: {value, onChange}, ...custom}) => (
     />
 );
 
+const AdaptedSelect = ({input: {value, onChange}, ...custom}) => (
+    <Select
+        value={value}
+        onChange={onChange}
+        input={<Input name="name" id="name-disabled" />}
+        {...custom}
+    >
+        <MenuItem value={'ru'}>Russian</MenuItem>
+        <MenuItem value={'en'}>English</MenuItem>
+    </Select>
+);
+
 const Search = () => (
     <form>
         <Field component={AdaptedTextField} name="page" label="Страница" margin="normal"/>
         <Field component={AdaptedTextField} name="articlesPerPage" label="Новостей на странице" margin="normal"/>
+        <Field component={AdaptedSelect} name="lang" label="Язык"/>
     </form>
 );
 
