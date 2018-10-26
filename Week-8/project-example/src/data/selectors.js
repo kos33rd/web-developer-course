855ef2acc53f47230a1bc58d6b867f22835a8edb
Konstantin Lobov <kos33rd@gmail.com>
2018-10-26 20:42:23 +0300

    Week-8 workshop - Bubbles chart with primitive selector

--- Week-8/project-example/src/data/selectors.js
+++ Week-8/project-example/src/data/selectors.js
@@ -1,4 +1,5 @@
-import { createSelector } from 'reselect'
+import {createSelector} from 'reselect'
+import _ from 'lodash'
 
 const newsSelector = createSelector(state => state.app.news, news => news || [])
 
@@ -21,14 +22,38 @@ export const photosSelector = createSelector(newsSelector,
 );
 
 export const hoursDistribution = createSelector(newsSelector,
-    (news) => {
+  (news) => {
 
-        const days = new Array(7).fill(0);
-        const daysDistribution = days.map(el => new Array(24).fill(0));
-        news.forEach(article => {
-            const date = new Date(article.published_at * 1000);
-            daysDistribution[date.getDay()][date.getHours()] += 1;
-        });
-        return daysDistribution;
-    }
+    const days = new Array(7).fill(0);
+    const daysDistribution = days.map(el => new Array(24).fill(0));
+    news.forEach(article => {
+      const date = new Date(article.published_at * 1000);
+      daysDistribution[date.getDay()][date.getHours()] += 1;
+    });
+    return daysDistribution;
+  }
 );
+
+export const wordsDistribution = createSelector(newsSelector,
+  (news) => {
+
+    return [
+      {label: 'CRM', value: 1},
+      {label: 'API', value: 1},
+      {label: 'Data', value: 1},
+      {label: 'Commerce', value: 1},
+      {label: 'AI', value: 3},
+      {label: 'Management', value: 5},
+      {label: 'Testing', value: 6},
+      {label: 'Mobile', value: 9},
+      {label: 'Conversion', value: 9},
+      {label: 'Misc', value: 21},
+      {label: 'Databases', value: 22},
+      {label: 'DevOps', value: 22},
+      {label: 'Javascript', value: 23},
+      {label: 'Languages / Frameworks', value: 25},
+      {label: 'Front End', value: 26},
+      {label: 'Content', value: 26},
+    ]
+  }
+)
--- Week-8/project-example/src/news/bubbles-chart.jsx
+++ Week-8/project-example/src/news/bubbles-chart.jsx
@@ -1,53 +1,48 @@
 import React from 'react';
 import BubbleChart from "@weknow/react-bubble-chart-d3";
+import {wordsDistribution} from '../data/selectors'
+import {connect} from "react-redux"
 
-const Chart = () =>
-  <BubbleChart
-    graph= {{
-      zoom: 1.1,
-      offsetX: -0.05,
-      offsetY: -0.01,
-    }}
-    width={1000}
-    height={800}
-    showLegend={true} // optional value, pass false to disable the legend.
-    legendPercentage={20} // number that represent the % of with that legend going to use.
-    legendFont={{
-      family: 'Arial',
-      size: 12,
-      color: '#000',
-      weight: 'bold',
-    }}
-    valueFont={{
-      family: 'Arial',
-      size: 12,
-      color: '#fff',
-      weight: 'bold',
-    }}
-    labelFont={{
-      family: 'Arial',
-      size: 16,
-      color: '#fff',
-      weight: 'bold',
-    }}
-    data={[
-      { label: 'CRM', value: 1 },
-      { label: 'API', value: 1 },
-      { label: 'Data', value: 1 },
-      { label: 'Commerce', value: 1 },
-      { label: 'AI', value: 3 },
-      { label: 'Management', value: 5 },
-      { label: 'Testing', value: 6 },
-      { label: 'Mobile', value: 9 },
-      { label: 'Conversion', value: 9 },
-      { label: 'Misc', value: 21 },
-      { label: 'Databases', value: 22 },
-      { label: 'DevOps', value: 22 },
-      { label: 'Javascript', value: 23 },
-      { label: 'Languages / Frameworks', value: 25 },
-      { label: 'Front End', value: 26 },
-      { label: 'Content', value: 26 },
-    ]}
-  />
+class Chart extends React.Component {
+  state = {
+    width: 1000
+  }
 
-export default Chart;
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+  }
+
+  componentDidMount() {
+    this.setState({width: this.ref.current.offsetWidth});
+  }
+
+  render() {
+    console.log(this.props.data)
+    return (
+      <div ref={this.ref}>
+        <BubbleChart
+          graph={{
+            zoom: 1.1,
+            offsetX: -0.05,
+            offsetY: -0.01,
+          }}
+          width={this.state.width}
+          height={800}
+          showLegend={true} // optional value, pass false to disable the legend.
+          legendPercentage={20} // number that represent the % of with that legend going to use.
+          legendFont={{family: 'Arial', size: 12, color: '#000', weight: 'bold'}}
+          valueFont={{family: 'Arial', size: 12, color: '#fff', weight: 'bold'}}
+          labelFont={{family: 'Arial', size: 16, color: '#fff', weight: 'bold'}}
+          data={this.props.data}
+        />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  data: wordsDistribution(state)
+})
+
+export default connect(mapStateToProps)(Chart);
