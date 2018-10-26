2bfbb5bb134ad4cd49bd8b94e1b247c5ec39ca2d
Konstantin Lobov <kos33rd@gmail.com>
2018-10-26 21:16:01 +0300

    Week-8 workshop - Words frequencies chart on lodash selectors

--- Week-8/project-example/src/data/selectors.js
+++ Week-8/project-example/src/data/selectors.js
@@ -37,23 +37,18 @@ export const hoursDistribution = createSelector(newsSelector,
 export const wordsDistribution = createSelector(newsSelector,
   (news) => {
 
-    return [
-      {label: 'CRM', value: 1},
-      {label: 'API', value: 1},
-      {label: 'Data', value: 1},
-      {label: 'Commerce', value: 1},
-      {label: 'AI', value: 3},
-      {label: 'Management', value: 5},
-      {label: 'Testing', value: 6},
-      {label: 'Mobile', value: 9},
-      {label: 'Conversion', value: 9},
-      {label: 'Misc', value: 21},
-      {label: 'Databases', value: 22},
-      {label: 'DevOps', value: 22},
-      {label: 'Javascript', value: 23},
-      {label: 'Languages / Frameworks', value: 25},
-      {label: 'Front End', value: 26},
-      {label: 'Content', value: 26},
-    ]
+    const data = _.chain(news)
+      .map(article => article.title)
+      .reduce((title, acc) => _.concat(_.words(title), acc), [])
+      .map(_.toLower)
+      .countBy(_.identity)
+      .map((count, word) => ({ label: word, value: count }))
+      .sortBy(['value'])
+      .reverse()
+      .take(10)
+      .value()
+
+    console.log(data)
+    return data
   }
 )
