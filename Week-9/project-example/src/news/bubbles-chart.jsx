3d20a4c764855fef7343640c8747aa6b48349767
Лобов Константин Евгеньевич <Lobov.K.Evg@sberbank.ru>
2018-11-02 11:41:38 +0300

    Week-9 - Remove console errors

--- Week-9/project-example/src/news/bubbles-chart.jsx
+++ Week-9/project-example/src/news/bubbles-chart.jsx
@@ -18,24 +18,25 @@ class Chart extends React.Component {
   }
 
   render() {
-    console.log(this.props.data)
     return (
       <div ref={this.ref}>
-        <BubbleChart
-          graph={{
-            zoom: 1.1,
-            offsetX: -0.05,
-            offsetY: -0.01,
-          }}
-          width={this.state.width}
-          height={800}
-          showLegend={true} // optional value, pass false to disable the legend.
-          legendPercentage={20} // number that represent the % of with that legend going to use.
-          legendFont={{family: 'Arial', size: 12, color: '#000', weight: 'bold'}}
-          valueFont={{family: 'Arial', size: 12, color: '#fff', weight: 'bold'}}
-          labelFont={{family: 'Arial', size: 16, color: '#fff', weight: 'bold'}}
-          data={this.props.data}
-        />
+          {!_.isEmpty(this.props.data) &&
+          <BubbleChart
+              graph={{
+                  zoom: 1.1,
+                  offsetX: -0.05,
+                  offsetY: -0.01,
+              }}
+              width={this.state.width}
+              height={800}
+              showLegend={true} // optional value, pass false to disable the legend.
+              legendPercentage={20} // number that represent the % of with that legend going to use.
+              legendFont={{family: 'Arial', size: 12, color: '#000', weight: 'bold'}}
+              valueFont={{family: 'Arial', size: 12, color: '#fff', weight: 'bold'}}
+              labelFont={{family: 'Arial', size: 16, color: '#fff', weight: 'bold'}}
+              data={this.props.data}
+          />
+          }
       </div>
     )
   }
